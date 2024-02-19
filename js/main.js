58bd4362024c518f690833789186495744a87230
Quirboy <quirdavluis297@gmail.com>
2024-02-19 11:38:30 +0100

    a working key
    a key to open a door an put in your inventory

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ document.getElementById("mainTitle").innerText = "Point and Click adventure game
 //gameWindow
 const GameWindow = document.getElementById("gameWindow");
 
+
+const inventoryBox  = document.getElementById("inventoryBox")
+const inventoryList  = document.getElementById("inventoryList")
 //FOREGROUND 
     const door1 = document.getElementById("door1")
 //maincharatcer
@@ -19,12 +22,27 @@ GameWindow.onclick = function(e)
 
     switch(e.target.id)
     {
+       
      case "door1":
         MainCharacter.style.backgroundColor ="#ffff00"
     door1.style.opacity = 0.7;
+    if(document.getElementById("key1") !== null){
+
+        console.log  ("FOUND KEY");
+        document.getElementById("key1").remove();
+        const Keyelement = document.createElement("li");
+        Keyelement.id= "inv-KEy"
+        Keyelement.innerText = "key";
+        inventoryList.appendChild(Keyelement);
+   
+    }
+        
+     break;
+     case "door2":
+
+     
 
      break;
-
      default:
         MainCharacter.style.backgroundColor ="#ffff00"
     door1.style.opacity = 1;
